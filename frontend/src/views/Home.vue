4c4507f1c36d0bb54d94c19fed0a4f770f79124e
Pokeya <pokeyboa.mystic@gmail.com>
2022-09-03 19:38:08 +0800

    Client configuration axios

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -10,6 +10,18 @@
 import Header from "../components/Header.vue"
 import Footer from "../components/Footer.vue"
 
+// 测试CORS的跨域配置是否有问题
+// import axios from "axios"
+// const http = axios.create()
+// 测试服务端的跨域是否配置成功
+// http.get("http://api.education-cloud.com:8000/home/test/").then(response=>{
+//   console.log(response.data);
+// })
+
+import http from "../utils/http";
+http.get("/home/test/").then(response=>{
+  console.log(response.data);
+})
 </script>
 
 <style scoped>
