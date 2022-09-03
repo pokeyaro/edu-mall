0e77341873ed9b686eb2620d5f7dc53e343dc9a0
Pokeya <pokeyboa.mystic@gmail.com>
2022-09-03 18:04:10 +0800

    The client displays the home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,15 @@
 <template>
-  <h1>首页</h1>
-  <el-row class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-    <el-rate v-model="store.value1" :colors="store.colors"> </el-rate>
-  </el-row>
+  <div class="home">
+    <Header></Header>
+
+    <Footer></Footer>
+  </div>
 </template>
 
 <script setup>
-import {reactive} from "vue";
-const store = reactive({
-  value1: null,
-  colors: ['#99A9BF', '#F7BA2A', '#FF9900'],
-})
+import Header from "../components/Header.vue"
+import Footer from "../components/Footer.vue"
+
 </script>
 
 <style scoped>
